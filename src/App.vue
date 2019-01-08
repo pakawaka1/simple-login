d55d134f5a1890000132ef5b827c9d5dfe31d0f3
David Lopaka Cervantes <davidcervantesdev@gmail.com>
2019-01-08 09:33:29 -0800

    fix: .gitignore Keiser favicon

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <div class="nav">
+      <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace>Logout</router-link>
+    </div>
+    <router-view @authenticated="setAuthenticated" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: 'App',
+  data() {
+    return {
+      authenticated: false,
+      admin: {
+        username: "david",
+        password: "keiser"
+      }
+    }
+  },
+  
+  mounted() {
+    if (!this.authenticated) {
+      this.$router.replace({ name: "login"});
+    }
+  },
+  methods: {
+    setAuthenticated(status) {
+      this.authenticated = status;
+    },
+    logout(){
+      this.authenticated = false;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background-color: #f0f0f0;
+  }
+  h1 {
+    padding: 0;
+    margin-top: 0;
+  }
+  .app {
+    width: 1024px;
+    margin: auto;
+  }
 </style>
---
+++ src/components/login.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="login">
+        <h1>Keiser Admin Login</h1>
+        <br>
+        <input type="text" name="username" v-model="input.username" placeholder="Username" />
+        <br>
+        <br>
+        <input type="password" name="password" v-model="input.password" placeholder="Password" />
+        <br>
+        <br>
+    
+        <button type="button" v-on:click="login()">Login</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data() {
+        return {
+            input: {
+                username: "",
+                password: ""
+            }
+        }
+    },
+    methods: {
+        login() {
+            if(this.input.username !="" && this.input.password !="") { 
+                if(this.input.username == this.$parent.admin.username && this.input.password == this.$parent.admin.password) {
+                    this.$emit("authenticated", true);
+                    this.$router.replace({ name: "secure"});
+                } else {
+                    alert("Your username and/or password is incorrect");
+                }
+            } else {
+                alert("Your username and password must exist");
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .login {
+        width: 400px;
+        border: 1px solid #ccc;
+        background-color: #fff;
+        margin: auto;
+        margin: 200px;
+        padding: 20px;
+    };
+
+    /* .keiser-font {
+        font-family: "Keiser";
+        color: #d10a2c;
+        font-size: 180%; 
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }; */
+
+</style>
+
